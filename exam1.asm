0f1c9efb73fdfb2e10be0eedb66aa1b5598d03b8
wolfsyntax <wolfsyntax@yahoo.com>
2016-07-04 12:16:02 +0800

    Update exam1.asm

--- exam1.asm
+++ exam1.asm
@@ -1,3 +1,9 @@
+;Prelime Examination in ICST105 - Computer Organization and Machine Language
+;Copyright (c) 2016. Wolf Syntax
+;Jayson D. Alpe (Wolf Syntax)
+;BSIT - 3
+;Ateneo de Naga University
+
 TITLE 	exam1.asm
 .model	small
 .stack 100H
@@ -15,14 +21,17 @@ TITLE 	exam1.asm
 		MOV AX, @data
 		MOV DS, AX
 		
+		;cout << 'Type the 4-digit year: $'
 		MOV AH, 09H
 		LEA DX, msg1
 		INT 21H
 		
+		;cin >> year
 		MOV AH, 3FH
 		MOV DX, OFFSET year
 		INT 21H
-
+		
+		;Reverse YEAR
 		MOV DL, year[0]
 		MOV reverse[4], DL
 
@@ -35,11 +44,12 @@ TITLE 	exam1.asm
 		MOV DL, year[3]
 		MOV reverse[1], DL
 		
-				
+		;cout << 'Reverse : '	
 		MOV AH, 09H
 		MOV DX, OFFSET msg2
 		INT 21H
 
+		;cout << reverse
 		MOV AH, 09H
 		MOV CX, 4
 		LEA DX, reverse+1
