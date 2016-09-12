7778eb285c6506de37e9b2bf6db62e1a0369585b
wolfsyntax <wolfsyntax@yahoo.com>
2016-09-12 23:07:11 +0800

    Update prob_c.cpp

--- prob_c.cpp
+++ prob_c.cpp
@@ -7,27 +7,4 @@
   
 **/
 
-#include <iostream>
-#include <set>
-
-using namespace std;
-
-int main()
-{
-	int test, n, x, y;
-	cin >> test;
-	while(test--)
-	{
-		cin >> n;
-
-		set<int> city;
-		for(int i = 0; i < n*2; i++){
-			cin >> x;
-			city.insert(x);
-			
-		}
-		cout << city.size() << endl;
-		
-	}
-	return 0;
-}
+HIDDEN
