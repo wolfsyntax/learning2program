7c571437049c0212784b23ea75d25ad2453c362a
wolfsyntax <wolfsyntax@yahoo.com>
2016-09-28 13:07:42 +0800

    Update prob_d.cpp

--- prob_d.cpp
+++ prob_d.cpp
@@ -7,4 +7,25 @@
   
 **/
 
-HIDDEN
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int test, x, n, gcd_koch; cin >> test;
+	while(test--)
+	{
+		cin >> n >> x;
+		
+		int a = x * pow(4,n);
+		int b = pow(3,n);
+		gcd_koch = __gcd(a,b);
+		cout << a/gcd_koch << "/" << b/gcd_koch << endl;
+		
+		
+	}
+	return 0;
+}
